87f8f0ef3d99f49e2487d60be39f2a43ea0927f5
Oleksii <githubbai@gmail.com>
2023-03-28 16:58:31 +0300

    hw27

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 function Accumulator() {
-    this.accum = 100;
+    this.accum = 0;
 }
 
+let element = document.getElementById('myElement');
 Accumulator.prototype.plusOne = function (plusNum) {
     this.accum += 1;
+    element.style.color = 'green';
 }
 
 Accumulator.prototype.minusOne = function (minusNum) {
     this.accum -= 1;
+    element.style.color = 'red';
 }
 
 Accumulator.prototype.clearAccum = function (clearNum) {
-    this.accum = 100;
+    this.accum = 0;
+    element.style.color = 'black';
 }
 
 Accumulator.prototype.printItems = function () {
-    console.log(this);
+
+    element.textContent = this.accum;
+
 }
 
 const accumulator = new Accumulator()
